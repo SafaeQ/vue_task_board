5ddfa1eec192d442d0597992bd5f9e29c3a430ba
SafaeQ <safaedani009@gmail.com>
2024-03-09 13:59:06 +0100

    add buttons

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavBar from "./components/NavBar.vue";
+import ButtonSection from "./components/ButtonSection.vue";
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <NavBar />
+  <ButtonSection />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/ButtonSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex flex-row justify-between p-8">
+    <div class="flex gap-3">
+      <button
+        class="w-32 bg-slightlyGray hover:bg-royalBlue font-semibold py-2 px-4 border border-gray92 rounded-lg"
+      >
+        <span class="text-dimGray">Add Task</span>
+      </button>
+      <button
+        class="w-32 bg-slightlyGray hover:bg-royalBlue text-dimGray font-semibold py-2 px-4 border border-gray92 rounded-lg"
+      >
+        Filter
+      </button>
+    </div>
+    <div>
+      <button
+        class="w-32 bg-slightlyGray hover:bg-royalBlue font-semibold py-2 px-4 border border-gray92 rounded-lg"
+      >
+        <span class="text-dimGray"> Share</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
